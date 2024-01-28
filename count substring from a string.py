0abe4986ba24ecbbadcb82b31019f7f644ab3770
Shai_Base <shaibase1328@gmail.com>
2024-01-28 13:51:02 +0800

    count method

---
+++ count substring from a string.py
@@ -0,0 +1,7 @@
+# Write a program to find how many times
+# substring “Emma” appears in the given string.
+
+# use the count method
+sentence = 'Emma is a good developer. Emma is a writer.'
+count = sentence.count('Emma')
+
